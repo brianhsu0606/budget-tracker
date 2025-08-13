60a0e0a8afb4933da8bcc8fc2893cd5cae7307c3
Brian Hsu <a0938406813@gmail.com>
2025-08-13 22:07:57 +0800

    feat: 前端 新增 fetchExpenses 函式，拿取後端預設資料

--- frontend/src/views/ExpenseView.vue
+++ frontend/src/views/ExpenseView.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+interface Expense {
+  date: string
+  name: string
+  category: string
+  amount: number
+}
+
+const expenseList = ref<Expense[]>([])
+
+const fetchExpenses = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/api/expenses')
+    expenseList.value = res.data.result
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  fetchExpenses()
+})
+</script>
 
 <template>
-  <div></div>
+  <el-card>
+    <el-table :data="expenseList">
+      <el-table-column prop="date" label="日期" />
+      <el-table-column prop="name" label="花費項目" />
+      <el-table-column prop="category" label="分類" />
+      <el-table-column prop="amount" label="金額" />
+    </el-table>
+  </el-card>
 </template>
 
 <style scoped lang="scss"></style>
