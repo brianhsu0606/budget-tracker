eb0d0f197bce135d674e757e2d1303c0190417e9
Brian Hsu <a0938406813@gmail.com>
2025-08-13 22:44:44 +0800

    feat: 新增月份篩選功能

--- frontend/src/views/ExpenseView.vue
+++ frontend/src/views/ExpenseView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
+import dayjs from 'dayjs'
 
 interface Expense {
   date: string
@@ -20,14 +21,26 @@ const fetchExpenses = async () => {
   }
 }
 
+// 月份篩選
+const currentMonth = ref<string>(dayjs().format('YYYY-MM'))
+const selectedMonth = ref<string>(currentMonth.value)
+const filteredExpenseList = computed(() => {
+  return expenseList.value.filter(
+    (expense) => dayjs(expense.date).format('YYYY-MM') === selectedMonth.value,
+  )
+})
+
 onMounted(() => {
   fetchExpenses()
 })
 </script>
 
 <template>
+  <header class="flex justify-end mb-4">
+    <el-date-picker v-model="selectedMonth" type="month" format="YYYY-MM" value-format="YYYY-MM" />
+  </header>
   <el-card>
-    <el-table :data="expenseList">
+    <el-table :data="filteredExpenseList">
       <el-table-column prop="date" label="日期" />
       <el-table-column prop="name" label="花費項目" />
       <el-table-column prop="category" label="分類" />
