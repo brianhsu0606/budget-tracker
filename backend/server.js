1a3754e34aaa839bb423b8edf6a2226b485b48bd
Brian Hsu <a0938406813@gmail.com>
2025-08-23 22:30:56 +0800

    feat: 資料庫連線，創建 db 資料夾、connectDB.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 import express from "express";
 import cors from "cors";
-import expensesRoutes from "./routes/expenses.js";
+import connectDB from "./db/connectDB.js";
+import expenseRoutes from "./routes/expenses.js";
 import incomeRoutes from "./routes/incomes.js";
 
 const app = express();
 const port = 3000;
 
+connectDB();
+
 app.use(cors()); // 跨域設定讓前端能請求
 app.use(express.json()); // 解析 JSON body
 
-app.use("/api/expenses", expensesRoutes);
+app.use("/api/expenses", expenseRoutes);
 app.use("/api/incomes", incomeRoutes);
-
-app.get("/", (req, res) => {
-  res.send("後端運行中");
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
