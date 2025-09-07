6035f9aaeb62458059066b40ec3dc42e51660ae5
Brian Hsu <a0938406813@gmail.com>
2025-09-07 16:16:25 +0800

    style: 調整 收入、支出 header RWD

--- frontend/src/views/IncomeView.vue
+++ frontend/src/views/IncomeView.vue
@@ -117,21 +117,23 @@ onMounted(() => {
 
 <template>
   <!-- Header 新增按鈕、月份篩選 -->
-  <el-card class="mb-4">
-    <header class="flex flex-col sm:flex-row justify-between sm:items-center gap-4">
-      <el-button @click="handleAdd" type="primary" class="text-lg h-10 p-4"> 新增收入 </el-button>
-      <h3 class="text-2xl font-semibold text-center order-first sm:order-none">收入分析</h3>
-      <el-date-picker
-        v-model="selectedMonth"
-        type="month"
-        format="YYYY-MM"
-        value-format="YYYY-MM"
-        :clearable="false"
-        class="text-lg"
-        size="large"
-      />
-    </header>
-  </el-card>
+  <div class="overflow-x-auto">
+    <el-card class="mb-4 min-w-[500px]">
+      <header class="flex flex-col sm:flex-row justify-between sm:items-center gap-4">
+        <el-button @click="handleAdd" type="primary" class="text-lg h-10 p-4"> 新增收入 </el-button>
+        <h3 class="text-2xl font-semibold text-center order-first sm:order-none">收入分析</h3>
+        <el-date-picker
+          v-model="selectedMonth"
+          type="month"
+          format="YYYY-MM"
+          value-format="YYYY-MM"
+          :clearable="false"
+          class="text-lg"
+          size="large"
+        />
+      </header>
+    </el-card>
+  </div>
 
   <!-- 內容區 -->
   <el-row :gutter="20" v-loading="isLoading" element-loading-text="載入中，請稍後...">
